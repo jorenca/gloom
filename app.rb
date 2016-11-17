4e0a0abf0168adbfedaf4e91f43bef578ccf11ff
jorenca <jv_mw@abv.bg>
2016-11-17 18:06:19 +0000

    GPIO is now operatable

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ set :port, 80
 set :bind, '0.0.0.0'
 
 class Io
+  RPi::GPIO.set_numbering :board
+
   def pins
     {
       output: {
@@ -16,8 +18,11 @@ class Io
   def initialize
     @state = {}
     pins.each do |type, pin_list|
-    pin_list.each { |name, pin|
-      @state[name] = 0 }
+      pin_list.each do |name, pin|
+        @state[name] = 0
+        RPi::GPIO.setup(pin, as: type)
+        RPi::GPIO.set_low(pin)
+      end
     end
   end
   
@@ -27,6 +32,7 @@ class Io
     p 'invoked set for ' + pin_name.to_s
     pin = pins[:output][pin_name]
     @state[pin_name] = value
+    value ? RPi::GPIO.set_high(pin) : RPi::GPIO.set_low(pin)
   end
 end
 
